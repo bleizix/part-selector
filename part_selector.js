a241d896c55050048832b877b6dc5f1c64bec71f
rusman1 <82220505+rusman1@users.noreply.github.com>
2022-06-13 22:59:50 +0500

    Add files via upload

---
+++ part_selector.js
@@ -0,0 +1,71 @@
+//@ts-check
+
+// NAME: time_setter
+// AUTHOR: Bleizix
+// DESCRIPTION: Set a диапазон that will be played.
+
+/// <reference path="../../spicetify-cli/globals.d.ts" />
+
+//   1. shortcut/button in context menu
+//   2. popup menu and manually set a time/set a time using bar
+//   3. play this part(easy)
+//   4. play again and again(cuz i do extension only for self using)
+
+
+let start_position = 0
+let end_position = 0
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+const msToMinutesSeconds = (ms) => {
+    const minutes = Math.floor(ms / 60000);
+    const seconds = ((ms % 60000) / 1000).toFixed(0);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`
+};
+
+function goto_start_time() {
+    Spicetify.Player.seek(start_position)
+    
+}
+// нотифы 
+async function shownotif_START_POSITION() {
+    
+    start_position = Spicetify.Player.getProgress()
+    
+    
+    let start_position_TEXT = msToMinutesSeconds(start_position)
+    Spicetify.showNotification(`Start position set to ${start_position_TEXT} `)
+    while (Spicetify.Player.isPlaying){
+        await sleep(1000);
+        let time = Spicetify.Player.getProgress();
+        if (time >= end_position) {
+            Spicetify.Player.seek(start_position)
+        }
+    }
+    
+}
+async function shownotif_END_POSITION(){
+
+    end_position = Spicetify.Player.getProgress()
+
+    let end_position_TEXT = msToMinutesSeconds(end_position)
+    Spicetify.showNotification(`End position set to ${end_position_TEXT}`)
+}
+function reset(){
+    start_position = 0
+    end_position = 0
+}
+Spicetify.Player.addEventListener("songchange", () => {
+    start_position = 0
+    end_position = 0
+});
+//some shit-code
+Spicetify.Mousetrap.bind('s', goto_start_time)
+
+Spicetify.Mousetrap.bind('d', shownotif_START_POSITION)
+Spicetify.Mousetrap.bind('f', shownotif_END_POSITION)
+
+
+
+
+// хуйня:::::      Set a two time: when song starts and when ends
